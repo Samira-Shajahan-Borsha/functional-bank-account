ed159411a0f700a45ddd13190dcc7690dab9257a
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2022-10-31 04:14:41 +0600

    updated deposit and balance

---
+++ js/banking.js
@@ -0,0 +1,25 @@
+//handle deposit event handler
+document.getElementById('deposit-button').addEventListener('click', function(){
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+
+    //get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    //update balance amount
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    
+    //clean input field
+    depositInput.value = '';
+
+    
+});
